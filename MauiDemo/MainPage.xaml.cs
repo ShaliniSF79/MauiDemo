8f7aeb1a989abc3c31ee6b1361f9e2633387e66f
ShaliniSF79 <161025378+ShaliniSF79@users.noreply.github.com>
2024-03-15 12:47:34 +0530

    Sample Added

---
+++ MauiDemo/MainPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿namespace MauiDemo
+{
+    public partial class MainPage : ContentPage
+    {
+        
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void TapGestureRecognizer_Tapped(object sender, TappedEventArgs e)
+        {
+            await DisplayAlert("Grid Tapped", "You tapped the grid!", "OK");
+        }
+
+        private async void TapGestureRecognizer_Tapped_1(object sender, TappedEventArgs e)
+        {
+            await DisplayAlert("Label Tapped", "You tapped the Label!", "OK");
+        }
+
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+            await DisplayAlert("Button Tapped", "You tapped the Button!", "OK");
+        }
+    }
+
+}
